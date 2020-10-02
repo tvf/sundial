94605d7c7f79c2004f471da85973065023e7f1f4
Thomas Foster <thomasvfoster@gmail.com>
2020-10-02 13:35:56 +0100

    add vector_to_sun(alt, azim)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import SunCalc from 'suncalc';
 
+import { vec3, mat4 } from 'gl-matrix';
+
+function setup_webgl() {
+    const canvas = document.querySelector('#gl_canvas');
+    const gl = canvas.getContext('webgl2');
+
+    if (!gl) {
+        alert('No WebGL2!');
+        return null;
+    }
+
+    return gl;
+}
+
+function vector_to_sun(altitude, azimuth) {
+
+    let x = -Math.sin(azimuth);
+    let y = -Math.cos(azimuth);
+
+    let horizontal_length = Math.cos(altitude);
+    let z = Math.sin(altitude);
+
+    return vec3.fromValues(horizontal_length * x, horizontal_length * y, z);
+}
+
 function main() {
+
+    const gl = setup_webgl();
 
     let today = new Date();
     let latitude = 52;
@@ -10,11 +37,14 @@ function main() {
 
     let sun_position_element = document.querySelector('#sun-pos');
     sun_position_element.textContent = JSON.stringify(sun_pos);
+
+    console.log("vector to sun: ",
+                vector_to_sun(sun_pos.altitude, sun_pos.azimuth));
 }
 
 // implementation plan
 //
-// [ ] given altitude + azimuth determine direction to sun in the
+// [X] given altitude + azimuth determine direction to sun in the
 //     X east Y north coordinate system
 //
 // [ ] load an OBJ sundial
@@ -28,6 +58,5 @@ function main() {
 // [ ] stencil shadows
 //
 // [ ] hook up the date time lat long controls
-//
 
 main();
